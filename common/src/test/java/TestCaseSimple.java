838d7097d7460a226536b375f9fded7c2be45cf3
linyongchao <yongchao.lin@fenbeitong.com>
2020-12-10 17:00:50 +0800

    1

---
+++ common/src/test/java/TestCaseSimple.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Title:
+ * Desc:
+ * Author: linyongchao
+ * Company: www.fenbeitong.com
+ * Date: 2020/12/98:21 PM
+ */
+public class TestCaseSimple {
+
+    @Test
+    public void test() {
+        int num = 2147483647 ;
+        num += 1 ;
+        System.out.println(num);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+    }
+}
