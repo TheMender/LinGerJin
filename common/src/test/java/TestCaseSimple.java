ae227be874094a02334d9fbe061669062b209898
linyongchao <yongchao.lin@fenbeitong.com>
2020-12-11 15:55:21 +0800

    1

--- common/src/test/java/TestCaseSimple.java
+++ common/src/test/java/TestCaseSimple.java
@@ -7,18 +7,29 @@ import java.util.List;
 /**
  * Title:
  * Desc:
- * Author: linyongchao
- * Company: www.fenbeitong.com
- * Date: 2020/12/98:21 PM
  */
 public class TestCaseSimple {
 
     @Test
     public void test() {
-        int num = 2147483647 ;
-        num += 1 ;
+        int num = 2147483647;
+        num += 1;
         System.out.println(num);
         System.out.println(Integer.MIN_VALUE);
         System.out.println(Integer.MIN_VALUE);
+    }
+
+    @Test
+    public void testd() {
+        int line = 6;
+        for (int i = 1; i < line; i++) {
+            for (int j = 1; j < line - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
