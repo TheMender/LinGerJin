616e7c9fc16aebdbec44261edf30875956ad6201
linyongchao <yongchao.lin@fenbeitong.com>
2020-12-14 15:16:19 +0800

    1

--- common/src/test/java/TestCaseSimple.java
+++ common/src/test/java/TestCaseSimple.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,5 +33,30 @@ public class TestCaseSimple {
             }
             System.out.println();
         }
+    }
+
+    @Test
+    public void testc() {
+        String str = "fenbeiguoji"+"train_query"+"20201214142030"+md5LowerCase32("f6rmpy2y59tqu7mptv94jg5vx5r6ll33");
+        System.out.println(md5LowerCase32(str));
+    }
+
+    public static String md5LowerCase32(String str) {
+        String restr = null;
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            byte[] digest = md5.digest(str.getBytes());
+            StringBuffer stringBuffer = new StringBuffer();
+            for (byte b : digest) {
+                int bt = b & 0xff;
+                if (bt < 16)
+                    stringBuffer.append(0);
+                stringBuffer.append(Integer.toHexString(bt));
+            }
+            restr = stringBuffer.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return restr;
     }
 }
